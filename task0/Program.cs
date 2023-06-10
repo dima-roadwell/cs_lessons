d0d2461e0c77d1efa09a865dc4732191bf0b3c99
dima-roadwell <aloun123@mail.ru>
2023-06-11 00:54:06 +0300

    seminar 1

---
+++ task0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int quater = num * num;
+
+Console.Write($"Num square {num}: {quater}");
---
+++ task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+
+Console.Write("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negativeNum = num * -1;
+
+for(int i = negativeNum; i <= num; i++)
+{
+    Console.Write(i + " ");
+}
