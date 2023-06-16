6fab3eb4615a5277938c49215e45fde75a4ee5b4
Dmitriy <aloun123@mail.ru>
2023-06-16 16:03:21 +0300

    task10

---
+++ task10_hw/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int outputNum = num / 10 % 10;
+
+System.Console.WriteLine($"{outputNum}");
