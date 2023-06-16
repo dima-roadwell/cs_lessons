7008a14fe8fac227cc8163ab8b3b8d8a609091b2
dima-roadwell <aloun123@mail.ru>
2023-06-16 21:22:40 +0300

    seminar 3

---
+++ task18/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+System.Console.Write("Input Quarter: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string[] quarterArr = {"x > 0, y > 0", 
+                        "x < 0, y > 0", 
+                        "x < 0, y < 0", 
+                        "x > 0, y < 0"
+                        };
+
+System.Console.WriteLine(quarterArr[num - 1]);
