b5474514a86fb6d261b287489e77168c5efc79e8
dima-roadwell <aloun123@mail.ru>
2023-06-29 16:50:00 +0300

    seminar 5

---
+++ task40/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и 
+// проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+int[] CreateArray(int countArrElems)
+{
+    int[] numArr = new int[countArrElems];
+
+    for(int i = 0; i < countArrElems; i++)
+    {
+        System.Console.Write($"Input num {i + 1}: ");
+        numArr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return numArr;
+}
+
+bool CheckSide(int[] sideArr)
+{
+    if(sideArr[0] < sideArr[1] + sideArr[2] && sideArr[1] < sideArr[0] + sideArr[2] && sideArr[2] < sideArr[0] + sideArr[1])
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Print(bool check)
+{
+    System.Console.WriteLine(check);
+}
+
+void Main()
+{
+    int[] arr = CreateArray(3);
+
+    Print(CheckSide(arr));
+}
+
+Main();
