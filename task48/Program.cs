6edc27f1bddeeeec942db230b1eeee2e840baec6
dima-roadwell <aloun123@mail.ru>
2023-07-04 12:30:12 +0300

    seminar 6

---
+++ task48/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] FillArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"Input el {i}; {j}: ");
+            matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Main()
+{
+    System.Console.Write("Input m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Input n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int[,] matrix = FillArray(m, n);
+    Print(matrix);
+}
+
+Main();
---
+++ task49/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int[,] FillArray(int m, int n)
+{
+    Random rnd = new Random();
+
+    int[,] matrix = new int[m, n];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void MakePowEvenElem(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = Convert.ToInt32(Math.Pow(matrix[i, j], 2));
+            }
+        }
+    }
+}
+
+void Print(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+void Main()
+{
+    System.Console.Write("Input m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Input n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int[,] matrix = FillArray(m, n);
+
+    Print(matrix);
+
+    MakePowEvenElem(matrix);
+
+    System.Console.WriteLine();
+    Print(matrix);
+}
+
+Main();
